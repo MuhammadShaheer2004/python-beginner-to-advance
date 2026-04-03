0dfa78a0a6174f9f40fb2dbf63f02e91620b854b
MuhammadShaheer2004 <shaheernasir999@gmail.com>
2026-04-03 20:07:56 +0500

    Added temperatur-converter program

--- Temperature/main.py
+++ Temperature/main.py
@@ -3,10 +3,10 @@ temp=float(input("Enter the temperature: "))
 unit=input("Is it in 'C' or 'F'?: ")
 
 if unit.upper()=="C":
-    temp=temp*(9/5)+32
+    temp=round(temp*(9/5)+32,2)
     print(f"Temperature in F: {temp}")
 elif unit.upper()=="F":
-    temp=(temp -32) * 5/9
+    temp=round((temp -32) * 5/9,2)
     print(f"Temperature in C: {temp}")
 else:
     print(f"{unit} is not a valid unit!")
